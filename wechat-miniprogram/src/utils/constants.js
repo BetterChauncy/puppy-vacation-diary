8f8872b2020a6faac8479ccad23328508cfc6ab8
liuxinfa <liuxinfa@bestpay.com.cn>
2026-06-10 12:46:28 +0800

    chore: switch back to dev mode for local testing

--- wechat-miniprogram/src/utils/constants.js
+++ wechat-miniprogram/src/utils/constants.js
@@ -1,6 +1,6 @@
-const DEV = false
+const DEV = true
 const DEV_URL = 'http://localhost:8000'
-const PROD_URL = 'https://puddy-2117605-1313120572.ap-shanghai.run.tcloudbase.com'
+const PROD_URL = 'https://your-production-domain.com'
 
 export const BASE_URL = DEV ? DEV_URL : PROD_URL
 export const MEDIA_URL = (key) => `${BASE_URL}/uploads/${key}`
